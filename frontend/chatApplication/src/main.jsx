7ec44da9ddc112e7304619bb5a712c80af995495
Tanusree <basuchoudhurytanusree@gmail.com>
2025-10-05 12:50:17 +0530

    Update main.jsx

--- frontend/chatApplication/src/main.jsx
+++ frontend/chatApplication/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-export const serverURL = "http://localhost:5000";
+export const serverURL = "https://chatly-chat-application-backend.onrender.com/";
 ReactDOM.createRoot(document.getElementById("root")).render(
  
   <React.StrictMode>
