f416ad28db5cfc2183bd5c661a7ce922605dac58
Tanusree <basuchoudhurytanusree@gmail.com>
2025-10-05 13:20:43 +0530

    Update Socket.js

--- backend/Socket/Socket.js
+++ backend/Socket/Socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chatly-chat-application-4iw5.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
