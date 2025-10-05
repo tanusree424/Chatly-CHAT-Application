c94fb654f7e7be819fb226c19247d44da030cf4e
Tanusree <basuchoudhurytanusree@gmail.com>
2025-10-05 14:05:33 +0530

    Update getOtherUsers.jsx

--- frontend/chatApplication/src/CustomHooks/getOtherUsers.jsx
+++ frontend/chatApplication/src/CustomHooks/getOtherUsers.jsx
@@ -8,19 +8,21 @@ const getOtherUsers = async () => {
     const dispatch = useDispatch();
     const { otherUsers } = useSelector((state) => state.user);
     useEffect(() => {
-        const fetchOtherUsers = async () => {
-            try {
-                const response = await axios.get(`${serverURL}/api/v1/users/all-users`, { withCredentials: true });
-                //console.log(response.data);
-                dispatch(setOtherUsers(response.data.users));
-                return response.data;
-            } catch (error) {
-                console.error("Error fetching other users:", error);
-                return null;
-            }
-        };
-        fetchOtherUsers();
-    }, [otherUsers , dispatch]);
+    const fetchOtherUsers = async () => {
+      try {
+        const response = await axios.get(`${serverURL}/api/v1/users/all-users`, {
+          withCredentials: true,
+        });
+        dispatch(setOtherUsers(response.data.users));
+      } catch (error) {
+        console.error("Error fetching other users:", error);
+      }
+    };
+
+    fetchOtherUsers();
+  }, [dispatch]);
+
+  return otherUsers;
 
 }
 
